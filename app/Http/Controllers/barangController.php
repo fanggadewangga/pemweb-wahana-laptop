842beca932074cb6e8d1158e012f78544ddeac74
fanggadewangga <fanggadewangga@gmail.com>
2023-05-26 13:25:38 +0700

    renaming model and controller

--- app/Http/Controllers/barangController.php
+++ app/Http/Controllers/barangController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\barang;
 use Illuminate\Http\Request;
 
-class barangController extends Controller
+class BarangController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
         $barang = barang::all();
-    
+
         return view('barang', ['barang' => $barang]);
     }
 }
