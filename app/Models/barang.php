56b00329b2a8dcedb3d235d05c15ae73b2dbbcb8
Avidhyana Sukma Valenta <avidhyana025@gmail.com>
2023-05-28 08:54:08 +0700

    add method getBarang

--- app/Models/barang.php
+++ app/Models/barang.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Barang extends Model
 {
     use HasFactory;
     protected $table = 'barang';
 
-    protected $filltable = ['id_barang', 'jenis_barang', 'merk_barang', 'tipe_barang', 'spesifikasi', 'tanggal_masuk_gudang', 'foto_barang', 'garansi', 'stok', 'harga_satuan', 'kelengkapan'];
+    protected $fillable = ['id_barang', 'jenis_barang', 'merk_barang', 'tipe_barang', 'spesifikasi', 'tanggal_masuk_gudang', 'foto_barang', 'garansi', 'stok', 'harga_satuan', 'kelengkapan'];
+
+    // Method untuk mengambil data barang
+    public function getBarang()
+    {
+        $result = DB::table($this->table)->get();
+        return $result;
+    }
 }
